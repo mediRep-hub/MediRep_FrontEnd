884a2af9bb91a7875b637428133becfa67b50980
gccberryb-alt <gccberryb@gmail.com>
2025-10-30 17:20:41 +0500

    update

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://medi-rep-back-end.vercel.app/";
+export const BASE_URL = "https://medi-rep-back-end.vercel.app";
 
 export const ENDPOINTS = {
   //   -----------Accounts-------------
