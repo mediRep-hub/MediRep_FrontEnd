bcd977adc6c5e6a373951e25da176e7d73155eaa
gccberryb-alt <gccberryb@gmail.com>
2025-10-30 17:17:32 +0500

    update

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,43 +1,43 @@
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = "https://medi-rep-back-end.vercel.app/";
 
 export const ENDPOINTS = {
   //   -----------Accounts-------------
-  ACCOUNTS_LOGIN: "api/admin/login",
-  ACCOUNTS_LOGOUT: "/api/admin/logout",
-  ACCOUNTS_GETALL: "/api/admin/getAll",
-  ACCOUNTS_UPDATE: "api/admin/updateAccount",
-  ACCOUNTS_DELETE: "/api/admin/deleteAccount",
-  ACCOUNTS_ADD: "/api/admin/register",
+  ACCOUNTS_LOGIN: "/admin/login",
+  ACCOUNTS_LOGOUT: "/admin/logout",
+  ACCOUNTS_GETALL: "/admin/getAll",
+  ACCOUNTS_UPDATE: "/admin/updateAccount",
+  ACCOUNTS_DELETE: "/admin/deleteAccount",
+  ACCOUNTS_ADD: "/admin/register",
 
   //   -----------Doctor-------------
-  DOCTOR_GET_ALL: "/api/doctor/getAllDoctor",
-  DOCTOR_ADD_DOCTOR: "/api/doctor/addDoctor",
-  DOCTOR_UPDATE: "/api/doctor/updateDoctor",
-  DOCTOR_DELETE: "/api/doctor/deleteDoctor",
+  DOCTOR_GET_ALL: "/doctor/getAllDoctor",
+  DOCTOR_ADD_DOCTOR: "/doctor/addDoctor",
+  DOCTOR_UPDATE: "/doctor/updateDoctor",
+  DOCTOR_DELETE: "/doctor/deleteDoctor",
 
   //   -----------Products-------------
-  PRODUCT_ADD_PRODUCT: "/api/product/addProduct",
-  PRODUCT_GET_ALL: "/api/product/getAllProducts",
-  PRODUCT_UPDATE: "/api/product/updateproduct",
-  PRODUCT_DELETE: "/api/product/deleteProduct",
+  PRODUCT_ADD_PRODUCT: "/product/addProduct",
+  PRODUCT_GET_ALL: "/product/getAllProducts",
+  PRODUCT_UPDATE: "/product/updateproduct",
+  PRODUCT_DELETE: "/product/deleteProduct",
 
   //   -----------call Reporting-------------
-  PRODUCTS_GET_ALL: "/api/callRepoeting/getAllReports",
+  PRODUCTS_GET_ALL: "/callRepoeting/getAllReports",
 
   //   -----------Manage MR-------------
-  MR_ADD_MR: "/api/manageMr/addMR",
-  MR_GET_ALL: "/api/manageMr/getAllMR",
-  MR_UPDATE: "/api/manageMr/updateMR",
-  MR_DELETE: "/api/manageMr/deleteMR",
+  MR_ADD_MR: "/manageMr/addMR",
+  MR_GET_ALL: "/manageMr/getAllMR",
+  MR_UPDATE: "/manageMr/updateMR",
+  MR_DELETE: "/manageMr/deleteMR",
 
   //   -----------Requisitions-------------
-  REQUISITIONS_ADD_REQUISITIONS: "/api/requisition/addRequisition",
-  REQUISITIONS_GET_ALL: "/api/requisition/getAllRequisition",
-  REQUISITIONS_UPDATE: "/api/requisition/updateRequisition",
-  REQUISITIONS_DELETE: "/api/requisition/deleteRequisition",
-  REQUISITIONS_GET_SINGLE: "/api/requisition/getSingleRequisition",
-  REQUISITIONS_UPDATEACCEPTED: "/api/requisition/updateAccepted",
+  REQUISITIONS_ADD_REQUISITIONS: "/requisition/addRequisition",
+  REQUISITIONS_GET_ALL: "/requisition/getAllRequisition",
+  REQUISITIONS_UPDATE: "/requisition/updateRequisition",
+  REQUISITIONS_DELETE: "/requisition/deleteRequisition",
+  REQUISITIONS_GET_SINGLE: "/requisition/getSingleRequisition",
+  REQUISITIONS_UPDATEACCEPTED: "/requisition/updateAccepted",
 
   //   -----------UploadFIle-------------
-  UPLOAD_FILE: "/api/uploadFile",
+  UPLOAD_FILE: "/upload/uploadFile",
 };
