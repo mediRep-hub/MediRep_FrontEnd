1f1c3fe1b82caa72b68b151afee3952b4a042139
gccberryb-alt <gccberryb@gmail.com>
2025-11-17 14:40:32 +0500

    update

--- src/Pages/Requisition/RequisitionDetail.tsx
+++ src/Pages/Requisition/RequisitionDetail.tsx
@@ -106,6 +106,7 @@ export default function RequisitionDetail() {
     try {
       setLoading(true);
       const res = await acceptRequisition(id);
+      console.log("🚀 ~ handleAccept ~ res:", res);
       notifySuccess("Requisition accepted!");
       await handleGetSingle(id);
     } catch (error: any) {
