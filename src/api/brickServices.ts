fa73c409921f7133a05c37e0e8b5b50275cdef4b
Amman <ammankashif7@gmail.com>
2026-02-10 16:39:41 +0500

    update

--- src/api/brickServices.ts
+++ src/api/brickServices.ts
@@ -1,29 +1,24 @@
 import { HTTP_CLIENT } from "../utils/httpClient";
 import { ENDPOINTS } from "./endpoints";
 
-// 👉 Get All Bricks
 export const getAllBricks = (brickName?: string) => {
   return HTTP_CLIENT.get(ENDPOINTS.REPORTING_GET_ALL, {
     params: { brickName },
   });
 };
 
-// 👉 Create Brick
 export const createBrick = (values: any) => {
   return HTTP_CLIENT.post(ENDPOINTS.REPORTING_ADD_REPORT, values);
 };
 
-// 👉 Update Brick
 export const updateBrick = (id: string, values: any) => {
   return HTTP_CLIENT.put(`${ENDPOINTS.REPORTING_UPDATE_REPORT}/${id}`, values);
 };
 
-// 👉 Delete Brick
 export const deleteBrick = (id: string) => {
   return HTTP_CLIENT.delete(`${ENDPOINTS.REPORTING_DELETE_REPORT}/${id}`);
 };
 
-// 👉 Reorder Doctor List (optional agar use karni ho)
 export const reorderDoctorList = (values: any) => {
   return HTTP_CLIENT.put(ENDPOINTS.REPORTING_REORDER_REPORT, values);
 };
